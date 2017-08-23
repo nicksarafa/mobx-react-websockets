e65cc8c105e07c6c4c00990cfc1caa42a8d2ee4a
nsarafa <nicksarafa@gmail.com>
2017-08-23 16:03:57 +0800

    styled-components example init

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import styled from 'styled-components'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <StyledLogo>
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
-        </div>
+        </StyledLogo>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+const StyledLogo = styled.div`
+  background: blue;
+`
+
+export default App

b429fb46545ed8b1d5193632f1490099b29e1678
nsarafa <nicksarafa@gmail.com>
2017-08-23 16:00:19 +0800

    BROKEN - routing store init && routing broken

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+<<<<<<< Updated upstream
 class App extends Component {
   render() {
     return (
@@ -19,3 +20,47 @@ class App extends Component {
 }
 
 export default App;
+=======
+const App = inject('routing')(
+  inject('exampleStore')(
+    observer(
+      class App extends Component {
+        componentDidMount() {
+          this.props.exampleStore._toggleIsExampleWorking()
+        }
+
+        _goToHell() {
+          this.props.routing.push(`/hell`)
+        }
+
+        render() {
+          return (
+            <div className="App">
+              {this.props.exampleStore.isThisExampleWorking
+                ? <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                  </div>
+                : false}
+              <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+              </p>
+              <button
+                onClick={() =>
+                  this.props.exampleStore._toggleIsExampleWorking()}
+              >
+                Toggle image
+              </button>
+              <button onClick={() => this._goToHell()}>
+                Go to Hell
+              </button>
+            </div>
+          )
+        }
+      },
+    ),
+  ),
+)
+
+export default App
+>>>>>>> Stashed changes
--- src/index.js
+++ src/index.js
@@ -4,10 +4,26 @@ import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import { Provider } from 'mobx-react'
+<<<<<<< Updated upstream
 import { ExampleStore } from './stores'
 
 const stores = {
   exampleStore: new ExampleStore(),
+=======
+import { ExampleStore, RouterStore } from './stores'
+import { Router } from 'react-router'
+import { Route } from 'react-router-dom'
+import { syncHistoryWithStore } from 'mobx-react-router'
+import createBrowserHistory from 'history/createBrowserHistory'
+
+const routingStore = new RouterStore()
+const browserHistory = createBrowserHistory()
+const history = syncHistoryWithStore(browserHistory, routingStore)
+
+const stores = {
+  exampleStore: new ExampleStore(),
+  routing: new RouterStore(),
+>>>>>>> Stashed changes
 }
 
 ReactDOM.render(
